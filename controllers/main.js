dec6d2f4fcc5bbff8b9ae12a3d4abae9f2487af0
Vitalii <vitalii0polishchuk@gmail.com>
2022-04-28 13:18:59 +0300

    first commit

---
+++ controllers/main.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new BadRequestError("Please provice email and password");
+  }
+
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
+};
+
+const dashboard = async (req, res) => {
+  console.log("dashboard");
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello ${req.user.username}`,
+    secret: `secret ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
